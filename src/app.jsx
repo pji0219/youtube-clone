60d9c0896ad9978ded9ae6c0915c91eb35c773bc
park jong ik <pji219@naver.com>
2021-02-17 09:03:52 +0900

    검색 기능 추가

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,31 @@
 import React, { useState, useEffect } from 'react';
+import style from './app.module.css';
+import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
 function App() {
   const [videos, setVideos] = useState([]);
 
+  // 함수 호출시 검색 API로부터 데이터 받아옴
+  function search(query) {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyD20Gd3wFas94NaVRLA3KO1vKyQkje7NV0`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        result.items.map((item) => ({ ...item, id: item.id.videoId }))
+      )
+      .then((items) => setVideos(items))
+      .catch((error) => console.log('error', error));
+  }
+
+  // 처음 앱이 시작었을 때 API로부터 가장 인기 많은 영상 데이터 받아옴
   useEffect(() => {
     const requestOptions = {
       method: 'GET',
@@ -18,7 +40,12 @@ function App() {
       .then((result) => setVideos(result.items))
       .catch((error) => console.log('error', error));
   }, []); // 빈 배열을 두번째 인자로 주면 한번만 마운트가 되었을 때만 코드 블럭 부분이 호출 된다.
-  return <VideoList videos={videos} />;
+  return (
+    <div className={style.app}>
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/search_header/search_header.jsx
@@ -0,0 +1,46 @@
+import React, { useRef } from 'react';
+import styles from './search_header.module.css';
+
+function SearchHeader(props) {
+  const inputRef = useRef(); // input에 입력된 값을 받아옴
+
+  function handleSearch() {
+    const value = inputRef.current.value;
+    props.onSearch(value);
+  }
+
+  function onClick() {
+    handleSearch();
+  }
+
+  function onKeyPress(event) {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
+  }
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img className={styles.img} src="/images/logo.png" alt="logo" />
+        <h1 className={styles.title}>moktube</h1>
+      </div>
+      <input
+        ref={inputRef}
+        className={styles.input}
+        type="search"
+        placeholder="검색"
+        onKeyPress={onKeyPress}
+      />
+      <button className={styles.button} type="submit" onClick={onClick}>
+        <img
+          className={styles.buttonImg}
+          src="/images/search.png"
+          alt="search"
+        />
+      </button>
+    </header>
+  );
+}
+
+export default SearchHeader;
