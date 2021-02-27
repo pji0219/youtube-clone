96b35c5bf44643acf8d1de5fbc9cb93c29d9f080
park jong ik <pji219@naver.com>
2021-02-27 23:43:05 +0900

    화면 디테일 만드는중2

--- src/components/video_detail/video_detail.jsx
+++ src/components/video_detail/video_detail.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './video_detail.module.css';
 
-function VideoDetail({ video }) {
+function VideoDetail({ video, video: { snippet } }) {
   return (
     <section className={styles.detail}>
       <iframe
@@ -9,10 +9,13 @@ function VideoDetail({ video }) {
         type="text/html"
         width="100%"
         height="500px"
-        src="https://www.youtube.com/embed/M7lc1UVf-VE"
+        src={`https://www.youtube.com/embed/${video.id}`}
         frameborder="0"
         allowfullscreen
       ></iframe>
+      <h2>{snippet.title}</h2>
+      <h3>{snippet.channelTitle}</h3>
+      <pre className={styles.description}>{snippet.description}</pre>
     </section>
   );
 }
