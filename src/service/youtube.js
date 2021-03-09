428680db1b9016e61cf059fbcb7b7577ccbc4a1e
park jong ik <pji219@naver.com>
2021-03-09 23:35:16 +0900

    유튜브 클론 코딩 완성

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -18,23 +18,19 @@ class Youtube {
             }
         });
         return response.data.items;
-        // const response = await fetch(
-        //     `/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
-        //     this.getRequestOptions
-        // );
-        // const result = await response.json();
-        // return result.items;
     }
 
     async search(query) {
 
-        const response = await this.youtube
-        // const response = await fetch(
-        //     `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
-        //     this.getRequestOptions
-        // );
-        // const result = await response.json();
-        // return result.items.map((item) => ({ ...item, id: item.id.videoId }));
+        const response = await this.youtube.get('search', {
+            params: {
+                part: 'snippet',
+                maxResults: 25,
+                q: query,
+                type: 'video',
+            }
+        });
+        return response.data.items.map((item) => ({ ...item, id: item.id.videoId }));
     }
 }
 
